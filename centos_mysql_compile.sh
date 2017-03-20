792b489fff3e45d2a72537d78588014876a68b80
AIdancer <aaronzlq@163.com>
2017-03-20 23:33:57 +0800

    Update centos_mysql_compile.sh

--- centos_mysql_compile.sh
+++ centos_mysql_compile.sh
@@ -1,1 +1,3 @@
+# We assume version.c is the source code which using mysql.
+# Please refer to http://zetcode.com/db/mysqlc/ for more information.
 gcc version.c -o version  `mysql_config --cflags --libs`
